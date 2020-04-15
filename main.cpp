520fa41e2194f556a7f51e84b026b606895d1b30
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-04-15 18:40:41 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Program that keeps track of various objects and variables in a Football Scoreboard.
+
+#include <iostream>
+using namespace std::
+
+int main() 
+{
+  std::cout << "Hello Scoreboard!\n";
+}
